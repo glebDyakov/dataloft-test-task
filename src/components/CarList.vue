6a5a284c5d59f701088f3a6e650509380c3eb5da
Gleb Dyakov <glebdyakov@Glebs-MacBook-Air.local>
2024-10-15 10:15:06 +0300

    add car-list component

---
+++ src/components/CarList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const cars = ref([
+  { id: 1, name: 'BMW', model: 'X5' },
+  { id: 2, name: 'Audi', model: 'A6' },
+])
+</script>
+
+<template>
+  <div class="container">
+    <h1>Car List</h1>
+    <div v-for="car in cars" :key="car.id" class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">{{ car.name }} {{ car.model }}</h5>
+        <router-link :to="`/car/${car.id}`" class="btn btn-primary">View Details</router-link>
+      </div>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
+import CarList from '../components/CarList.vue'
+import CarDetails from '../components/CarDetails.vue'
+import PostDetails from '../components/PostDetails.vue'
 
-const routes = []
+const routes = [
+  { path: '/', component: CarList },
+  { path: '/car/:id', component: CarDetails },
+  { path: '/post/:id', component: PostDetails }
+]
 
-const router = createRouter({ routes, history: createMemoryHistory() })
+const router = createRouter({ routes, history: createWebHistory() })
 export default router
