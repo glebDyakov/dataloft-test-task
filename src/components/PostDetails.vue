0b465bfa4bb75a5dd1a10e4a06d57b688ec7c8a1
Gleb Dyakov <glebdyakov@Glebs-MacBook-Air.local>
2024-10-15 09:51:45 +0300

    add post-details component

---
+++ src/components/PostDetails.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const post = ref({ id: 1, title: 'Post 1', content: 'This is the first post.' },)
+const comments = ref([
+  { id: 1, text: 'Great post!' },
+  { id: 2, text: 'Very informative.' },
+])
+</script>
+
+<template>
+  <div class="container">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+    <h4>Comments</h4>
+    <ul>
+      <li v-for="comment in comments" :key="comment.id">{{ comment.text }}</li>
+    </ul>
+  </div>
+</template>
